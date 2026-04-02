af76cde5df64890c4371a444ae8ada0788d13144
synnbaek <100paek100@naver.com>
2026-04-02 19:36:15 +0900

    feat/3에 처방전 이미지 업로드 및 로컬 저장 로직 구현을 추가

--- src/main/java/com/carelink/drug/repository/DrugRepository.java
+++ src/main/java/com/carelink/drug/repository/DrugRepository.java
@@ -9,4 +9,7 @@ import java.util.Optional;
 public interface DrugRepository extends JpaRepository<DrugEntity, Long> {
     Optional<DrugEntity> findByDrugId(Long drugId);
     List<DrugEntity> findByNameContaining(String name);
+
+    // 추가: itemSeq로 약을 찾는 메서드
+    Optional<DrugEntity> findByItemSeq(String itemSeq);
 }
