7c649b83e7f5de3f23ecca8192ffb584ef3fe578
tember8003 <yuchan8003@naver.com>
2026-04-05 00:54:15 +0900

    Feat: merge

---
+++ src/main/java/com/carelink/recommendation/dto/DepartmentRecommendRequest.java
@@ -0,0 +1,10 @@
+package com.carelink.recommendation.dto;
+
+import lombok.Getter;
+
+@Getter
+public class DepartmentRecommendRequest {
+
+    private Object symptoms;
+    private String customDescription;
+}
